f25537c4e7fe700b9487ce3f99fad1e9070033ad
moloudii <mhmmdalinaz@gmail.com>
2020-10-30 00:45:59 +0330

    create popup and successfully created task

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// --------------------------------------------------------
+// ------------ Begin Fixed Header ---------
+// --------------------------
 window.onscroll = function () {
     fixedHeader()
 };
@@ -10,6 +13,15 @@ function fixedHeader() {
         $(".columns-section").css("margin-top", "4.5em");
     }
 }
+// --------------------------------------------------------
+// -------------- End Fixed Header ---------
+// --------------------------
+
+
+// --------------------------------------------------------
+// ------------ Begin Scrolling Link -------
+// --------------------------
+
 $(document).ready(function () {
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault()
@@ -22,14 +34,63 @@ $(document).ready(function () {
         )
     });
 });
-function allowDrop(ev){
+
+// --------------------------------------------------------
+// ------------ End Scrolling Link ---------
+// --------------------------
+
+
+// --------------------------------------------------------
+// ------------ Begin Drop Down ------------
+// --------------------------
+
+function allowDrop(ev) {
     ev.preventDefault();
 }
-function dragCardTask(ev){
+
+function dragCardTask(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
-function dropCardTask(ev){
+
+function dropCardTask(ev) {
     ev.preventDefault();
     let data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+// --------------------------------------------------------
+// -------------- End Drop Down ------------
+// --------------------------
+
+
+// --------------------------------------------------------
+//  Begin Corresponding to the pop-up window
+// --------------------------
+let popup = document.getElementById('popup');
+let addTaskButton = document.getElementById('add-task-day');
+let titlePopup = document.getElementById('title-popup');
+
+function showPopup(nameTask) {
+    popup.style.display = "grid";
+    titlePopup.innerHTML = "Create New Task " + nameTask;
+    document.getElementById('name-task').focus();
+
+    // to find div father button 
+    let div = document.getElementById(`${nameTask}`);
+    console.log(div);
+}
+
+function closePopup(){
+    popup.style.display = "none";
+}
+
+window.addEventListener("click", function (event) {
+    if (event.target == popup) {
+        // popup.style.display = "none";
+        closePopup();
+    }
+});
+
+// --------------------------------------------------------
+//    End Corresponding to the pop-up window
+// --------------------------
